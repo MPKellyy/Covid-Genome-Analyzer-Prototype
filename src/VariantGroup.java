d290ef6ccb9a10779a7ca64bd0aef254edb41198
MPKellyy <cybernationnews@gmail.com>
2021-09-03 19:10:28 -0500

    Cleaned up comments and added additional documentation to code.

--- src/VariantGroup.java
+++ src/VariantGroup.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class for comparing similarities between genomes of a viral strain
+ */
 public class VariantGroup {
     /**
      * NOTES:
@@ -10,22 +13,35 @@ public class VariantGroup {
      */
     private ArrayList<String> genomes;
 
+    /**
+     * Constructor for VariantGroup
+     */
     public VariantGroup() {
         genomes = new ArrayList<String>();
     }
 
-    //Add genome
+    /**
+     * Allows user to add a single genome string to structure
+     * @param genome string sequence of a genome
+     */
     public void addGenome(String genome) {
         genomes.add(genome);
     }
 
-    //Add genomes
+    /**
+     * Allows user to add a list of genome strings
+     * @param inputGenomes ArrayList of genome strings
+     */
     public void addGenomes(ArrayList<String> inputGenomes) {
         for(String genome: inputGenomes)
             genomes.add(genome);
     }
 
-    //Remove genomes
+    /**
+     * If the genome is stored in structure, it will be deleted
+     * @param genome genome to be deleted
+     * @return
+     */
     public boolean removeGenome(String genome) {
         if(genomes.contains(genome)) {
             genomes.remove(genome);
@@ -35,39 +51,60 @@ public class VariantGroup {
         return false;
     }
 
-    //Check if in VariantGroup
+    /**
+     * Checks if structure has the inputted genome
+     * @param genome string of genome being looked for
+     * @return
+     */
     public boolean hasGenome(String genome) {
         return genomes.contains(genome);
     }
 
-    //Get all genomes
+    /**
+     * Returns a clone of all stored genomes
+     * @return
+     */
     public ArrayList<String> getGenomes() {
         return (ArrayList<String>) genomes.clone();
     }
 
-    //Clears stored genomes
+    /**
+     * Creates a new ArrayList to store genome strings in
+     */
     public void clearGenomes() {
         genomes = new ArrayList<String>();
     }
 
-    //Generate Similarity Sequence
+    /**
+     * Function used to identify similarities between genomes currently stored
+     * Note: differences are saved as '-' within the returned Character ArrayList
+     * @return returns ArrayList of characters compared. Elements with '-' mark differences found
+     * at a given index
+     */
     public ArrayList<Character> generateSimilaritySequence() {
-        //Similarity Logic
+        //Setting first saved genome as the string being compared
         String base = genomes.get(0);
+        //Keeps track if all genomes match at a specified position in their sequence
         boolean allMatch = true;
+        //List that will keep track of nucleotide positions
         ArrayList<Character> unfilteredSequence = new ArrayList<Character>();
 
+        //Looping through every element of first genome
         for(int i = 0; i < base.length(); i++) {
 
+            //Looping through all genomes other than the first (which is being compared to)
             for(int j = 1; j < genomes.size(); j++) {
+                //If at any point at least one genome does not match, comparisons are stopped for current position
                 if(genomes.get(j).charAt(i) != base.charAt(i)) {
                     allMatch = false;
                     break;
                 }
             }
 
+            //If all matched, add the matching character to the Character ArrayList
             if(allMatch)
                 unfilteredSequence.add(base.charAt(i));
+            //Else, mark difference with a '-'
             else
                 unfilteredSequence.add('-');
 
@@ -77,59 +114,43 @@ public class VariantGroup {
         return unfilteredSequence;
     }
 
-    //Generate Unfiltered Similarity String
+    /**
+     * Generates a string of the genome comparisons unfiltered (with '-' marking differences)
+     * @return
+     */
     public String getUnfilteredSimString() {
-        //Getting String unfiltered result
+        //String that will hold nucleotides from genome comparison similarity
         String unfilteredResult = "";
 
-        for(Character letter: generateSimilaritySequence()) {
-            unfilteredResult += letter;
+        //Concatenating nucleotide to sequence string
+        for(Character nucleotide: generateSimilaritySequence()) {
+            unfilteredResult += nucleotide;
         }
 
         return unfilteredResult;
     }
 
-    //Generate Similarity Map (filtered)
-    public Map<Character, Integer> getFilteredSimMap() {
-        ArrayList<Character> similaritySequence = generateSimilaritySequence();
-        Map<Character, Integer> filteredSimMap = new HashMap<Character, Integer>();
-
-        for(int k = 0; k < similaritySequence.size(); k++)
-        {
-            System.out.println(similaritySequence.get(k) + ": " + k);
-        }
-
-        for(int i = 0; i < similaritySequence.size(); i++) {
-            if(similaritySequence.get(i) != '-')
-                filteredSimMap.put(similaritySequence.get(i), i);
-        }
-
-        return filteredSimMap;
-    }
-
-    //Find sequences and orders
+    /**
+     * Finds similar nucleotide sequences between same strains and their locations within genomes
+     * @return
+     */
     public Map<String, String> findSimilaritiesOrdered() {
-        //get arraylist
-        //Create Map
-        //String for sequence order i-j
-        //String for sequence itself
-        //Start index
-        //Loop through list until last character is read
-            //If next is not current
-                //Save string and order to map
-                //Set sequence and order strings to empty
-                //Set start to next index
-            //else
-                //sequence += current char
-
+        //Saves generated unfilteredNucleotide Sequence
         ArrayList<Character> unfilteredSequence = generateSimilaritySequence();
+        //Map for saving sequences to genomes respective locations
         Map<String, String> similaritiesOrdered = new HashMap<String, String>();
+        //String for sequence location on genome
         String order = "";
+        //String for saving nucleotide
         String sequence = "";
+        //Integer for saving position of the start of a new sequence
         int start = 0;
+        //Boolean for keeping track of consecutive sequencing
         boolean needStart = unfilteredSequence.get(0) == '-';
 
+        //Loop through unfiltered nucleotide similarity sequence
         for(int i = 0; i < unfilteredSequence.size(); i++) {
+            //Case for when a '-' is first encountered
             if(unfilteredSequence.get(i) == '-' && !needStart) {
                 order = Integer.toString(start) + "-" + Integer.toString(i-1);
                 similaritiesOrdered.put(order, sequence);
@@ -138,16 +159,19 @@ public class VariantGroup {
                 needStart = true;
                 start = -1;
             }
+            //Case for when a '-' is followed by a valid nucleotide
             else if(needStart && unfilteredSequence.get(i) != '-') {
                 start = i;
                 sequence += unfilteredSequence.get(i);
                 needStart = false;
             }
+            //Case for when a nucleotide is followed by another nucleotides
             else if (unfilteredSequence.get(i) != '-'){
                 sequence += unfilteredSequence.get(i);
             }
         }
 
+        //Ensuring final sequence gets saved if possible
         if(start != -1) {
             order = Integer.toString(start) + "-" + Integer.toString(unfilteredSequence.size()-1);
             similaritiesOrdered.put(order, sequence);
--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class main {
     public static void main(String[] args) {
-        //Setting up ArrayList
+        //Setting up ArrayLists
         ArrayList<String> wordList = new ArrayList<String>();
         String word1 = "-almon";
         wordList.add(word1);
@@ -15,32 +15,22 @@ public class main {
         String word4 = "-ilmon";
         wordList.add(word4);
 
+        //Setting VariantGroup
         VariantGroup test = new VariantGroup();
         test.addGenomes(wordList);
 
-//        int index = 0;
-//        for(Character letter: test.generateSimilaritySequence()) {
-//            System.out.println(letter + ": " + index);
-//            index++;
-//        }
 
-        //System.out.println(test.getUnfilteredSimString());
-
-
-//        Map<Character, Integer> filteredSequence = test.getFilteredSimMap();
-//        for(Character key: filteredSequence.keySet()) {
-//            System.out.println(key + ": " + filteredSequence.get(key));
-//        }
-
+        //Verifying Results
         Map<String, String> similarities = test.findSimilaritiesOrdered();
 
         for(String key: similarities.keySet()) {
             System.out.println(similarities.get(key) + ": " + key);
         }
 
-
-        //TODO: Generate Map of Pair of index locations as value and consecutive Strings as value
-
+        //TODO: Be able to handle strings of different lengths
+        //TODO: Read strings from a textfile
+        //TODO: Find differences?
+        //TODO: Look for differences in similarities
 
     }
 }
